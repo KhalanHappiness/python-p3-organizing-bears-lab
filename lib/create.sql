75719077bbd47998ba11eb3817da4517b9556299
happiness <happinesskhalan@gmail.com>
2025-05-21 07:40:43 +0300

    Organizing bears lab complete

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE bears (
+ id INT PRIMARY KEY,
+ name TEXT,
+ age INTEGER,
+ sex TEXT,
+ color TEXT,
+ temperament TEXT,
+ alive INT
+    
+ )
+
+ 
---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Mr. Chocolate', 3, 'M', 'Brown', 'sanguin', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Rowdy', 2, 'F', 'Dark Brown', 'melancholic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Tabitha', 5, 'F', 'Black', 'phlegmatic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Sergeant Brown', 6, 'M', 'White', 'choleric', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Melissa', 1, 'F', 'Brown', 'melancholic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Grinch', 2, 'M', 'Cream', 'phlegmatic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Wendy', 5, 'F', 'Red', 'sanguin', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(NULL, 1, 'M', 'Blue', 'melancholic', 1);
